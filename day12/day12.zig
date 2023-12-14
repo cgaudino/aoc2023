3ab7813a9955bc11275363f1591539e2f25febd6
Chris Gaudino <cgaudino@gmail.com>
2023-12-13 21:46:42 -0700

    [day12] implement part 2

--- day12/day12.zig
+++ day12/day12.zig
@@ -12,18 +12,26 @@ pub fn main() !void {
     defer alloc.free(input);
 
     var groupsBuffer: [32]u32 = .{1} ** 32;
+    var unfoldBuffer: []u8 = try alloc.alloc(u8, 2048);
+    defer alloc.free(unfoldBuffer);
+    var cache = std.AutoHashMap(Query, u64).init(alloc);
+    defer cache.deinit();
 
     var linesIter = std.mem.tokenizeScalar(u8, input, '\n');
     var partOne: u64 = 0;
+    var partTwo: u64 = 0;
     while (linesIter.next()) |line| {
-        partOne += try processLine(line, &groupsBuffer);
+        cache.clearRetainingCapacity();
+        partOne += try processLine(line, unfoldBuffer, &groupsBuffer, &cache, 1);
+        cache.clearRetainingCapacity();
+        partTwo += try processLine(line, unfoldBuffer, &groupsBuffer, &cache, 5);
     }
-    std.debug.print("Part One: {d}\n", .{partOne});
+    std.debug.print("Part One: {d}\nPart Two: {d}\n", .{ partOne, partTwo });
 }
 
-fn processLine(line: []const u8, buffer: []u32) !u64 {
+fn processLine(line: []const u8, unfoldBuffer: []u8, buffer: []u32, cache: *std.AutoHashMap(Query, u64), foldCount: usize) !u64 {
     const separatorIndex = std.mem.indexOfScalar(u8, line, ' ').?;
-    const springs = line[0..separatorIndex];
+    const springs = unfold(u8, line[0..separatorIndex], unfoldBuffer, '?', foldCount);
 
     var groupsIter = std.mem.tokenizeScalar(u8, line[(separatorIndex + 1)..], ',');
     var groupCount: usize = 0;
@@ -32,40 +40,105 @@ fn processLine(line: []const u8, buffer: []u32) !u64 {
         groupCount += 1;
     }
 
-    var groups = buffer[0..groupCount];
+    var groups = unfold(u32, buffer[0..groupCount], buffer, null, foldCount);
 
-    return findValidConfigurations(springs, groups, 0, false, null);
+    const query = .{ .springIndex = 0, .groupIndex = 0, .groupSize = 0, .inGroup = false, .replacementChar = null };
+    const result = findValidConfigurations(query, springs, groups, cache);
+
+    return result;
 }
 
-fn findValidConfigurations(springs: []const u8, groups: []u32, groupSize: isize, inGroup: bool, replacementChar: ?u8) u64 {
-    const char = if (replacementChar != null) replacementChar.? else springs[0];
+fn unfold(comptime T: type, source: []const T, buffer: []T, separator: ?T, count: usize) []T {
+    if (source.ptr != buffer.ptr) {
+        @memcpy(buffer[0..source.len], source);
+    }
+
+    var i: usize = 1;
+    var copyDestIndex = source.len;
+    while (i < count) : (i += 1) {
+        if (separator) |s| {
+            buffer[copyDestIndex] = s;
+            copyDestIndex += 1;
+        }
+        @memcpy(buffer[copyDestIndex .. copyDestIndex + source.len], source);
+        copyDestIndex += source.len;
+    }
+    return buffer[0..copyDestIndex];
+}
+
+fn findValidConfigurations(query: Query, springs: []const u8, groups: []u32, cache: *std.AutoHashMap(Query, u64)) u64 {
+    if (cache.get(query)) |result| {
+        return result;
+    }
+
+    const char = if (query.replacementChar != null) query.replacementChar.? else springs[query.springIndex];
     switch (char) {
         '.' => {
-            var newGroup = groups;
-            if (inGroup) {
-                if (groupSize != groups[0]) {
+            var newGroupIndex = query.groupIndex;
+            if (query.inGroup) {
+                if (query.groupSize != groups[query.groupIndex]) {
                     return 0;
                 }
-                if (groups.len == 1) {
-                    return if (std.mem.indexOfScalar(u8, springs, '#') == null) 1 else 0;
+                if (query.isLastGroup(groups)) {
+                    return if (std.mem.indexOfScalar(u8, springs[query.springIndex..], '#') == null) 1 else 0;
                 }
-                newGroup = groups[1..];
+                newGroupIndex += 1;
             }
-            if (springs.len == 1) {
+            if (query.isLastSpring(springs)) {
                 return 0;
             }
-            return findValidConfigurations(springs[1..], newGroup, 0, false, null);
+            const result = findValidConfigurations(query.next(newGroupIndex, 0, false), springs, groups, cache);
+            cache.put(query, result) catch {};
+            return result;
         },
         '#' => {
-            if (springs.len == 1) {
-                return if (groups.len == 1 and groups[0] == groupSize + 1) 1 else 0;
+            if (query.isLastSpring(springs)) {
+                return if (query.isLastGroup(groups) and groups[query.groupIndex] == query.groupSize + 1) 1 else 0;
             }
-            return findValidConfigurations(springs[1..], groups, groupSize + 1, true, null);
+            const result = findValidConfigurations(query.next(query.groupIndex, query.groupSize + 1, true), springs, groups, cache);
+            cache.put(query, result) catch {};
+            return result;
         },
         '?' => {
-            return findValidConfigurations(springs, groups, groupSize, inGroup, '.') +
-                findValidConfigurations(springs, groups, groupSize, inGroup, '#');
+            return findValidConfigurations(query.replaceChar('.'), springs, groups, cache) +
+                findValidConfigurations(query.replaceChar('#'), springs, groups, cache);
         },
         else => unreachable,
     }
 }
+
+const Query = struct {
+    springIndex: usize,
+    groupIndex: usize,
+    groupSize: isize,
+    inGroup: bool,
+    replacementChar: ?u8,
+
+    fn next(self: Query, groupIndex: usize, groupSize: isize, inGroup: bool) Query {
+        return .{
+            .springIndex = self.springIndex + 1,
+            .groupIndex = groupIndex,
+            .groupSize = groupSize,
+            .inGroup = inGroup,
+            .replacementChar = null,
+        };
+    }
+
+    fn replaceChar(self: Query, char: u8) Query {
+        return .{
+            .springIndex = self.springIndex,
+            .groupIndex = self.groupIndex,
+            .groupSize = self.groupSize,
+            .inGroup = self.inGroup,
+            .replacementChar = char,
+        };
+    }
+
+    fn isLastSpring(self: Query, springs: []const u8) bool {
+        return self.springIndex == springs.len - 1;
+    }
+
+    fn isLastGroup(self: Query, groups: []u32) bool {
+        return self.groupIndex == groups.len - 1;
+    }
+};
