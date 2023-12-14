6787977ee6c2578110eee1bce34e4f999778e947
Chris Gaudino <cgaudino@gmail.com>
2023-12-13 22:48:52 -0700

    [day13] implement part 2

--- day13/day13.zig
+++ day13/day13.zig
@@ -16,45 +16,56 @@ pub fn main() !void {
 
     var linesIter = std.mem.splitScalar(u8, input, '\n');
     var partOne: usize = 0;
+    var partTwo: usize = 0;
     while (linesIter.next()) |line| {
         if (line.len == 0) {
-            partOne += findVerticalReflectionLine(rows.items);
-            partOne += findHorizontalReflectionLine(rows.items) * 100;
+            partOne += findVerticalReflectionLine(rows.items, 0);
+            partOne += findHorizontalReflectionLine(rows.items, 0) * 100;
+            partTwo += findVerticalReflectionLine(rows.items, 1);
+            partTwo += findHorizontalReflectionLine(rows.items, 1) * 100;
             rows.clearRetainingCapacity();
             continue;
         }
         try rows.append(line);
     }
 
-    std.debug.print("Part One: {d}\n", .{partOne});
+    std.debug.print("Part One: {d}\nPart Two: {d}\n", .{ partOne, partTwo });
 }
 
-fn findVerticalReflectionLine(rows: [][]const u8) usize {
+fn findVerticalReflectionLine(rows: [][]const u8, smudgeTarget: usize) usize {
     var result = rows[0].len - 1;
-    outer: while (result > 0) : (result -= 1) {
+    while (result > 0) : (result -= 1) {
         var i: usize = 0;
+        var smudges: usize = 0;
         while (result >= i + 1 and result + i < rows[0].len) : (i += 1) {
             for (rows) |row| {
                 if (row[result - 1 - i] != row[result + i]) {
-                    continue :outer;
+                    smudges += 1;
                 }
             }
         }
-        return result;
+        if (smudges == smudgeTarget) {
+            return result;
+        }
     }
     return 0;
 }
 
-fn findHorizontalReflectionLine(rows: [][]const u8) usize {
+fn findHorizontalReflectionLine(rows: [][]const u8, smudgeTarget: usize) usize {
     var result = rows.len - 1;
-    outer: while (result > 0) : (result -= 1) {
+    while (result > 0) : (result -= 1) {
         var i: usize = 0;
+        var smudges: usize = 0;
         while (result >= i + 1 and result + i < rows.len) : (i += 1) {
-            if (!std.mem.eql(u8, rows[result - 1 - i], rows[result + i])) {
-                continue :outer;
+            for (rows[result - 1 - i], rows[result + i]) |a, b| {
+                if (a != b) {
+                    smudges += 1;
+                }
             }
         }
-        return result;
+        if (smudges == smudgeTarget) {
+            return result;
+        }
     }
     return 0;
 }
